cceb6329ea35a93f885a95d59e81fa35c1596cc3
Paulo Gutierrez <paulo@databricks.com>
2020-06-04 15:14:59 +0900

    Create mlflow_quick_start.py

---
+++ mlflow_quick_start.py
@@ -0,0 +1,27 @@
+# Import various libraries including matplotlib, sklearn, mlflow
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from itertools import cycle
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import lasso_path, enet_path
+from sklearn import datasets
+
+import mlflow
+import mlflow.sklearn
+
+diabetes = datasets.load_diabetes()
+X = diabetes.data
+y = diabetes.target
+
+# Create pandas DataFrame for sklearn ElasticNet linear_model
+Y = np.array([y]).transpose()
+d = np.concatenate((X, Y), axis=1)
+cols = ['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6', 'progression']
+data = pd.DataFrame(d, columns=cols)
